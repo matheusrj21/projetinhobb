1ddbe696e94c856ea00c951ffa5830a2357c0a6a
matheus <socialfastpromo@gmail.com>
2024-12-22 20:03:20 -0300

    Mensagem descritiva do que foi alterado

--- servedor2.js
+++ servedor2.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const path = require('path');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
+
+// Middleware para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Endpoint para buscar dados do perfil
 app.get('/profile/:username', async (req, res) => {
     const { username } = req.params;
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']  // Adicionando a opção para evitar o erro
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] // Adiciona essas opções
     });
     const page = await browser.newPage();
 
@@ -21,26 +24,24 @@ app.get('/profile/:username', async (req, res) => {
         const profileUrl = `https://www.instagram.com/${username}/`;
         await page.goto(profileUrl, { waitUntil: 'networkidle2' });
 
-        // Espera o seletor da foto do perfil aparecer e pega a URL da imagem
-        await page.waitForSelector('img[alt^="Foto do perfil"]');
-        const profilePic = await page.$eval('img[alt^="Foto do perfil"]', (img) => img.src);
-
-        // Extração da descrição do perfil
         await page.waitForSelector('meta[name="description"]');
         const description = await page.$eval('meta[name="description"]', el => el.content);
 
-        res.json({
-            username,
-            description,
-            profilePic, // Passa a URL da imagem do perfil
-        });
+        res.json({ username, description });
     } catch (error) {
+        console.error('Erro ao buscar perfil do Instagram:', error);
         res.json({ error: 'Erro ao buscar os dados do perfil.' });
     } finally {
         await browser.close();
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+// Teste simples de funcionamento
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando no Railway!');
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
